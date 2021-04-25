fab0cf1cd406ab0208581c1ea5c67b719d330a4f
staszewski <kamilstaszewski95@gmail.com>
2021-04-25 15:04:00 +0200

    Start rendering content dynamically

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+  <article>
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <nuxt-content :document="article" />
+  </article>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+
+    return { article }
+  },
+}
+</script>
