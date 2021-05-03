3c5a4c489c866ca938af1aa60aab3e587c673f60
staszewski <kamilstaszewski95@gmail.com>
2021-05-03 18:13:12 +0200

    Start navigation

--- components/heading/heading.vue
+++ components/heading/heading.vue
@@ -1,17 +1,20 @@
 <template>
-  <header class="phone:py-6 tablet:py-8">
-    <nav>
-      <h1 class="text-red-400 text-3xl font-bold">
-        <NuxtLink to="/">Notes</NuxtLink>
-      </h1>
-      <NuxtLink to="/blog">Blog</NuxtLink>
-    </nav>
+  <header class="flex phone:py-6 tablet:py-8">
+    <h1 class="text-red-400 text-3xl font-bold">
+      <NuxtLink to="/">Notes</NuxtLink>
+    </h1>
+    <Navigation />
   </header>
 </template>
 
 <script>
+import Navigation from '~/components/navigation/navigation.vue'
+
 export default {
   name: 'heading',
+  components: {
+    Navigation,
+  },
 }
 </script>
 
---
+++ components/navigation/navigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="navigationItem in navigationItems" :key="navigationItem.url">
+        <NuxtLink :to="navigationItem.url"> {{ navigationItem.name }}</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface NavigationItem {
+  name: string
+  url: string
+}
+
+interface State {
+  navigationItems: NavigationItem[]
+}
+
+const navigationItems: NavigationItem[] = [
+  { name: 'Blog', url: '/blog' },
+  { name: 'About', url: '/about' },
+]
+
+export default Vue.extend({
+  name: 'navigation-component',
+  data(): State {
+    return {
+      navigationItems,
+    }
+  },
+})
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
+
+li {
+  margin: 0 1rem;
+}
+
+a {
+  padding: 1rem;
+}
+</style>
