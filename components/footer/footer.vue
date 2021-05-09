88dd9bfb692597ae95dfc8cce98291f83a1ca128
staszewski <kamilstaszewski95@gmail.com>
2021-05-09 19:46:50 +0200

    Contact

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -3,6 +3,7 @@
     <div id="container">
       <a href="https://github.com/staszewski">Github</a>
       <a href="https://twitter.com/k_staszewski">Twitter</a>
+      <a href="mailto:kamilstaszewski95@gmail.com">Contact</a>
       <p class="text-gray-500 text-xs">
         © 2020-present Kamil Staszewski. All Rights Reserved.
       </p>
